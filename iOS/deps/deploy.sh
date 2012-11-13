5bce9aa02a05c6d5479ae8c25868c78541438330
PIlin <ilin.pa@gmail.com>
2012-11-13 17:29:23 +0100

    Little patches to sdl and sdl_ttf build script. It will fix architecture of the build.

--- iOS/deps/deploy.sh
+++ iOS/deps/deploy.sh
@@ -5,3 +5,6 @@ echo "Downloading dependencies\n"
 hg clone http://hg.libsdl.org/SDL
 hg clone http://hg.libsdl.org/SDL_ttf
 git clone git://github.com/cdave1/freetype2-ios.git libfreetype
+
+cd SDL; patch -p1 < ../sdl.diff; cd ..
+cd SDL_ttf; patch -p1 < ../sdl_ttf.diff; cd ..

9bac31643406be595c12e6225fd822c0ee8665b4
PIlin <ilin.pa@gmail.com>
2012-11-26 00:22:02 +0100

    Fixed broken sound in release configuration. Fixes #18
    Problem was in some compiler optimization. It appears, that line
    (unsigned char)wave->phaseacc
    return differnt values with optimizations enabled, whic leads to bugs to sound generation.
    Use of volatile is really smelly, but it works.

--- sound/pxt.cpp
+++ sound/pxt.cpp
@@ -23,26 +23,29 @@
 #define MODEL_SIZE			256
 #define PXCACHE_MAGICK		'PXC1'
 
-// gets the next byte from wave "wave", scales it by the waves volume, and places result in "out".
-// x * (y / z) = (x * y) / z
-#define GETWAVEBYTE(wave, out)	\
-{	\
-	if (wave->model_no != MOD_WHITE)	\
-	{		\
-		out = wave->model[(unsigned char)wave->phaseacc];	\
-	}		\
-	else	\
-	{	\
-		out = white[wave->white_ptr];		\
-		if (++wave->white_ptr >= WHITE_LEN) wave->white_ptr = 0;	\
-	}	\
-	out *= wave->volume;			\
-	out /= 64;	\
-}
 
 
 #define WHITE_LEN		22050
 int8_t white[WHITE_LEN];
+
+// gets the next byte from wave "wave", scales it by the waves volume, and places result in "out".
+// x * (y / z) = (x * y) / z
+inline void GETWAVEBYTE(stPXWave* wave, int& out)
+{
+    if (wave->model_no != MOD_WHITE)
+    {
+        volatile unsigned char index = (unsigned char)wave->phaseacc;
+        out = wave->model[index];
+    }
+    else
+    {
+        out = white[wave->white_ptr];
+        if (++wave->white_ptr >= WHITE_LEN) wave->white_ptr = 0;
+    }
+    out *= wave->volume;
+    out /= 64;
+}
+
 
 // the final sounds ready to play (after pxt_PrepareToPlay)
 static struct

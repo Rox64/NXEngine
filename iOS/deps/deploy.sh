363b116896d531b6caaa8be856cbbfb94cf403c9
Pavel Ilin <ilin.pa@gmail.com>
2014-01-23 18:10:41 +0200

    Fix #40 - Cannot Compile in Xcode 5
    Add patches to fix SDL and libfreetype problems under Xcode 5.
    Update deploy.sh to clone specific versions of SDL and SDL_ttf and apply new patches.

--- iOS/deps/deploy.sh
+++ iOS/deps/deploy.sh
@@ -2,9 +2,10 @@
 
 echo "Downloading dependencies\n"
 
-hg clone http://hg.libsdl.org/SDL
-hg clone http://hg.libsdl.org/SDL_ttf
+hg clone -r 7501 http://hg.libsdl.org/SDL
+hg clone -r 239 http://hg.libsdl.org/SDL_ttf
 git clone git://github.com/cdave1/freetype2-ios.git libfreetype
 
-cd SDL; patch -p1 < ../sdl.diff; cd ..
+cd SDL; patch -p1 < ../SDL_gles2funcs.diff; cd ..
 cd SDL_ttf; patch -p1 < ../sdl_ttf.diff; cd ..
+cd libfreetype; patch -p1 < ../libfreetype.diff; cd ..

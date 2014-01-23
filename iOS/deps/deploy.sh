618b79d3dc3921844815c8e6738b62c13f54871c
Pavel Ilin <ilin.pa@gmail.com>
2014-01-23 23:13:41 +0200

    Update to SDL_ttf 2.0.12
    Remove libfreetype dependency because SDL_ttf also brings freetype 2.4.12.

--- iOS/deps/deploy.sh
+++ iOS/deps/deploy.sh
@@ -3,9 +3,9 @@
 echo "Downloading dependencies\n"
 
 hg clone -r release-2.0.1 http://hg.libsdl.org/SDL
-hg clone -r 239 http://hg.libsdl.org/SDL_ttf
-git clone git://github.com/cdave1/freetype2-ios.git libfreetype
+hg clone -r release-2.0.12 http://hg.libsdl.org/SDL_ttf
 
 cd SDL; patch -p1 < ../SDL_gles2funcs.diff; cd ..
 cd SDL_ttf; patch -p1 < ../sdl_ttf.diff; cd ..
-cd libfreetype; patch -p1 < ../libfreetype.diff; cd ..
+cd SDL_ttf; patch -p1 < ../sdl2_ttf_apinames.diff; cd ..
+
